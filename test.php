4268c1b48f7c997f606eb384c8c883f904c16677
moinnaik06 <moinnaik06@gmail.com>
2021-07-03 19:07:13 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,15 @@
-abc
+<?php
+$servername = "103.53.43.82";
+$username = "vrgamlic_events";
+$password = "Vrgamezone123@";
+$dbname = "vrgamlic_events";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+} else{
+	echo "Connection Successful";
+}
+?>
